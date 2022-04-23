d604d5af97db34b131945cd0b53a9c0bb705b680
Gabrielle <gabrielle01.spence@gmail.com>
2022-04-23 13:16:40 -0700

    added event listeners to change color based on what part of palette is clicked

---
+++ index.js
@@ -0,0 +1,23 @@
+let selectedColor = 'blue'
+const painting = document.querySelector('.painting')
+let blueColor = document.querySelector('#blue')
+let redColor = document.querySelector('#red')
+let yellowColor = document.querySelector('#yellow')
+let greenColor = document.querySelector('#green')
+
+blueColor.addEventListener('click', function(){
+    selectedColor = 'blue'
+})
+redColor.addEventListener('click', function(){
+    selectedColor = 'red'
+})
+yellowColor.addEventListener('click', function(){
+    selectedColor = 'yellow'
+})
+greenColor.addEventListener('click', function(){
+    selectedColor = 'green'
+})
+
+painting.addEventListener('click', function(e){
+    e.target.style.backgroundColor = selectedColor
+})
